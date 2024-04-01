9226d4fcb9a713dcdb904643dcf89fa8ffdf5b38
Tkomch <a1458675007@live.com>
2024-04-01 13:03:03 +0800

    增加MLP多头网络

---
+++ Nets/mlp_mutil_head_5layer.py
@@ -0,0 +1,34 @@
+import torch.nn as nn
+import sys
+sys.path.append('..')
+from config import *
+
+## the pre-trained model
+class MLPNet(nn.Module):
+    def __init__(self):
+        super(MLPNet, self).__init__()
+        self.feature = nn.Sequential(
+            nn.Linear(pca_dim, 64), # need change 700->288
+            nn.ReLU(),
+            nn.Linear(64, 128),
+            nn.ReLU(),
+            nn.Linear(128, 256),
+            nn.ReLU(),
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+        )
+
+        self.head = nn.ModuleList([nn.Sequential(
+            nn.Linear(64, output_k),
+            nn.Softmax(dim=1)
+            ) for _ in range(num_sub_heads)])
+    
+    # how the network runs
+    def forward(self, input):
+        result = []
+        x = self.feature(input)
+        for i in range(num_sub_heads):
+            result.append(self.head[i](x))
+        return result
