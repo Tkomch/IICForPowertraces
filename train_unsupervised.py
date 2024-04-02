c2f2bc94c32f1173cee2a973fdd5970189a1a501
Tkomch <a1458675007@live.com>
2024-04-02 14:27:25 +0800

    修缮无监督训练

--- train_unsupervised.py
+++ train_unsupervised.py
@@ -3,14 +3,11 @@ import os
 import torch
 import torch.nn as nn
 # 可视化张量图片
-# from torchvision.transforms import ToPILImage
-# from DataTransers.ImageTranser import DoubleBatchTransform
 from DataTransers.TraceTranser import TripleBatchTransform
 from IIC_Loss import IIC_Loss
 from DataLoaders.LoadPartASCAD import Datasetloader
 from config import *
 # 引入网络结构
-from Nets.cnn_multi_head import CNNNet
 from utils import setup_seed
 from tqdm import tqdm
 
@@ -20,14 +17,24 @@ setup_seed(seed)
 def train():
     train_loader, _ = Datasetloader(train_data_path)(bs, is_shuffle, dataset_mode, left, right)
     # 更改网络结构在这里
-    model = CNNNet().to(device)
+    print("将使用%s网络结构进行训练" % net_structure)
+    if (net_structure == 'cm3'):
+        from Nets.cnn_multi_head import CNNNet
+        model = CNNNet().to(device)
+    if (net_structure == 'mm5'):
+        from Nets.mlp_mutil_head_5layer import MLPNet
+        model = MLPNet().to(device)
+    if (net_structure == 'mm7'):
+        from Nets.mlp_mutil_head_7layer import MLPNet
+        model = MLPNet().to(device)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
     iic_loss_fn = IIC_Loss()
     iic_loss_fn = iic_loss_fn.to(device)
 
-    transformer2 = TripleBatchTransform()
-    transformer2 = transformer2.to(device)
+    # transformer2 = TripleBatchTransform()
+    # transformer2 = transformer2.to(device)
     # 将每个epoch的loss保存下来
     loss_list = []
     for epoch in range(num_epochs):
@@ -35,28 +42,20 @@ def train():
         running_loss = 0.0
         print(f"epoch: {epoch}")
         for i, data in enumerate(tqdm(train_loader), 0):
-            inputs, labels = data
-            inputs, labels = inputs.to(device), labels.to(device)
-            # 用于测试数据变换效果
-            # show(inputs[0]).show()
-            # inputs_test1, inputs_test2 = transformer2(inputs)
-            # show(inputs_test1[0]).show()
-            # show(inputs_test2[0]).show()
-            # exit()
+            inputs, inputs_trans, labels = data
+            inputs, inputs_trans, labels = inputs.to(device), inputs_trans.to(device), labels.to(device)
             # 通过数据变换对象对数据进行变换
-            inputs_trans2, inputs_trans3 = transformer2(inputs)
+            # inputs_trans = transformer2(inputs)
             optimizer.zero_grad()
             outputs1 = model(inputs)
-            outputs2 = model(inputs_trans2)
-            outputs3 = model(inputs_trans3)
+            outputs2 = model(inputs_trans)
             #计算IID损失
             loss = 0.0
             loss_no_lamb = 0.0
             for i in range(num_sub_heads):
-                loss1, loss1_no_lamb = iic_loss_fn(outputs1[i], outputs2[i])
-                loss2, loss2_no_lamb = iic_loss_fn(outputs1[i], outputs3[i])
-                loss += (loss1 + loss2)
-                loss_no_lamb += (loss1_no_lamb + loss2_no_lamb)
+                loss1, _ = iic_loss_fn(outputs1[i], outputs2[i])
+                loss += loss1
+                # loss_no_lamb += (loss1_no_lamb + loss2_no_lamb)
             loss.backward()
             optimizer.step()
             running_loss += loss.item()
